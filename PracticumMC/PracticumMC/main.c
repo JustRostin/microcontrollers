e87a9233f4d96b7a098bd82700cddcc93cf37504
Robin <luijtenrobin@gmail.com>
2022-02-02 11:30:18 +0100

    initial commit

---
+++ PracticumMC/PracticumMC/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 02-Feb-22 11:17:20 AM
+ *  Author: robin
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
