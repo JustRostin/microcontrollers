a7b40bb421710021dfb78718f532c0f03841b20e
Luca <Luca.1999@hotmail.com>
2022-02-02 13:54:05 +0100

    Opzet Opdracht 2C

---
+++ Opdrachtb3/Opdrachtb3/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 2/2/2022 1:51:42 PM
+ *  Author: luca1
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
