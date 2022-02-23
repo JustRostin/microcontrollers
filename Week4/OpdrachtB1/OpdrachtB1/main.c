9361ad78ccf0d9432a14d7bb6c659c37e833c340
Luca <Luca.1999@hotmail.com>
2022-02-23 13:29:06 +0100

    W4 Opdracht B1

---
+++ Week4/OpdrachtB1/OpdrachtB1/main.c
@@ -0,0 +1,18 @@
+/*
+ * OpdrachtB1.c
+ *
+ * Created: 23-2-2022 12:54:44
+ * Author : luca1
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
