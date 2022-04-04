2f488949d87c0de6300d51735f4a056f4670c588
Luca <Luca.1999@hotmail.com>
2022-04-04 15:08:38 +0200

    Laatste wijzigingen

--- eindopdracht/Eindopdracht/Eindopdracht/BM.h
+++ eindopdracht/Eindopdracht/Eindopdracht/BM.h
@@ -5,7 +5,10 @@
  *  Author: luca1
  */ 
 
+#ifndef BM
+#define BM
 #include "ServoDriver.h"
 
 void BM_init(void);
 void BM_handle_data(void);
+#endif
--- eindopdracht/Eindopdracht/Eindopdracht/main.c
+++ eindopdracht/Eindopdracht/Eindopdracht/main.c
@@ -12,18 +12,8 @@
 #include "ServoDriver.h"
 #include "BM.h"
 
-int speed = 0;
-
 int main(void)
 {
-	
-	
-	
-
-	/* Enable receiver and transmitter */
-	//UCSR3B = (1<<RXEN2);
-	/* Set frame format: 8data, 1stop bit */
-	//UCSR3C = (0<<USBS3)|(3<<UCSZ03);
 	SD_init();
 	SD_set_drive_direction(STILL);
 	BM_init();
